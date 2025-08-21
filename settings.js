f8c4f9c721401489d404aeca9529ca4a28052572
Itsperkins <saliurahman4@gmail.com>
2025-08-21 04:49:38 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
-  author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  packname: 'Perkins Bot',
+  author: '‎Bot',
+  botName: "Perkins Bot",
+  botOwner: 'Perkins', // Your name
+  ownerNumber: '2349128263157', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
